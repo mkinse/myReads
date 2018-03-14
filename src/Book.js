d2b107c051c9a991c725248b1847e03ce41647ca
mkinse <zarafamark@gmail.com>
2018-03-13 21:22:07 -0500

    fix: converge books with or without shelves

--- src/Book.js
+++ src/Book.js
@@ -1,33 +1,58 @@
 import React from 'react'
-import * as BooksAPI from './BooksAPI'
+import PropTypes from 'prop-types'
 
 //import PropTypes from 'prop-types'
 
 class Book extends React.Component {
+    static propTypes = {
+        onShelfChange: PropTypes.func.isRequired
+      }
 
-    onDoAThing = (event) =>
+    handleShelfChange = (book, shelf) =>
     {
-        BooksAPI.update(this.props.book, event).then(function(res){
-            console.log('the res is: ',res);
-        });
+        console.log('You passed a book?:', book);
+        console.log('You passed a shelf?:', shelf);
+        //TODO: send all of this up to parent element.
+        this.setState({ [shelf]: shelf })
+
+        this.props.onShelfChange(book, shelf);
     }
 
     render()
       {
-          const { book } = this.props.book
+          const { book } = this.props
+
           return(
           <div className="book">
             <div className="book-top">
+            {book.imageLinks.thumbnail ? (
               <div className="book-cover" style={{ width:128, height: 193, backgroundImage:`url(${this.props.book.imageLinks.thumbnail})` }} ></div>
-              <div className="book-shelf-changer">
-                <select onChange={(event) => this.onDoAThing(event.target.value)}>
+          ):(
+              <div className="book-cover" style={{ width:128, height: 193, backgroundImage:`url()` }} ></div>
+          )}
+
+              {this.props.book.shelf ? (
+                  <div className="book-shelf-changer">
+                  <select value={this.props.book.shelf} onChange={(event) => this.handleShelfChange(book, event.target.value)}>
+                   <option value="none" disabled>Move to...</option>
+                   <option value="currentlyReading">Currently Reading</option>
+                   <option value="wantToRead">Want to Read</option>
+                   <option value="read">Read</option>
+                   <option value="">None</option>
+                 </select>
+                 </div>
+             ) : (
+                 <div className="add-book-button">
+                 <select value={this.props.book.shelf ? this.props.book.shelf : 'none'} onChange={(event) => this.handleShelfChange(book, event.target.value)}>
                   <option value="none" disabled>Move to...</option>
                   <option value="currentlyReading">Currently Reading</option>
                   <option value="wantToRead">Want to Read</option>
                   <option value="read">Read</option>
-                  <option value="none">None</option>
                 </select>
-              </div>
+                </div>
+            )}
+
+
             </div>
             <div className="book-title">{this.props.book.title}</div>
             <div className="book-authors">{this.props.book.authors}</div>
