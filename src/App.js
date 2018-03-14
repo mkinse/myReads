7d62d93b65d16b0af05990b4ed0f84ac20a1979d
mkinse <zarafamark@gmail.com>
2018-03-13 21:21:28 -0500

    fix: redo search to deal with error conditions

--- src/App.js
+++ src/App.js
@@ -1,66 +1,109 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Route, Link } from 'react-router-dom'
-
 import * as BooksAPI from './BooksAPI'
 import BookList from './BookList'
-
+import Book from './Book'
 import './App.css'
 
 class BooksApp extends React.Component {
-
     state = {
       books: [],
-
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+      foundBooks: [],
+      query: ''
   }
 
   componentDidMount() {
-      BooksAPI.getAll().then((books) => {
-          this.setState({ books })
-      })
+          BooksAPI.getAll().then((books) => {
+              console.log('Here are the books from cdu?:', books);
+              this.setState({ books })
+          })
   }
 
+  loadBooks() {
+    BooksAPI.getAll().then( allBooks => {
+      this.setState({
+        books: allBooks
+      });
+    });
+  }
+
+  handleShelfChange = (book, shelf) => {
+      BooksAPI.update(book, shelf).then( result =>{
+            this.loadBooks();
+                })
+  }
+
+  searchForBooks = query => {
+      this.setState({ ['foundBooks']: [] });
+      console.log('query is:', query);
+      BooksAPI.search(query, 20).then(
+          searchResult => {
+              if(searchResult.error)
+              {
+                  console.log('error????', searchResult);
+                  this.setState({ foundBooks: [] })
+              }else{
+                    console.log('no error...', searchResult);
+                   this.setState({ foundBooks: searchResult })
+               }
+            }, errorMsg => {
+                console.log('error!', errorMsg);
+                this.setState({ foundBooks: [] })
+            })
+          }
+
+  updateQuery = (query) => {
+      this.setState({ query: query.trim() });
+      this.searchForBooks(query);
+    }
+
   render() {
+      const { query } = this.state
+      const { books } = this.state
+      const { foundBooks } = this.state
+
     return (
       <div className="app">
-      <Route exact path='/search' render={() => (
+
+      <Route exact path='/search' render={({ history }) => (
           <div className="search-books">
             <div className="search-books-bar">
               <Link to='/' className="close-search">Close</Link>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
+                <input type="text" placeholder="Search by title or author"
+                    value={query}
+                    onChange={(event) => this.updateQuery(event.target.value)}/>
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+              {
+                  foundBooks.length > 0 ?
+                   foundBooks.map(book =>
+                      <li key={book.id}>
+                      <Book book={book} onShelfChange={this.handleShelfChange}/>
+                      </li>
+                )
+                :
+                    query ?
+                        <div>No results found for '{query}'.</div>
+                        :
+                        <div>No results found.</div>
+               }
+              </ol>
             </div>
           </div>
-      )}/>
-      <Route exact path='/' render={({ history }) => ( <div className="list-books">
+      )}
+      />
+
+      <Route exact path='/' render={({ history }) => (
+         <div className="list-books">
          <div className="list-books-title">
            <h1>MyReads</h1>
          </div>
-
-         <BookList books={this.state.books}/>
-
+         <BookList books={this.state.books} handleShelfChange={this.handleShelfChange} />
          <div className="open-search">
-           <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+           <Link to='/search' >Add a book</Link>
          </div>
        </div>)}/>
       </div>
