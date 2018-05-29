a2bdfc74c85f576e42ea0904482c8126a0766bf4
Mark Kinsella <zarafamark@gmail.com>
2018-05-28 20:06:50 -0500

    feat: show placeholder if not thumbnmail, blank div if no author

--- src/Book.js
+++ src/Book.js
@@ -24,7 +24,11 @@ class Book extends React.Component {
           return(
           <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width:128, height: 193, backgroundImage:`url(${ book.imageLinks.thumbnail})` }} ></div>
+              { book.imageLinks ? (
+                  <div className="book-cover" style={{ width:128, height: 193, backgroundImage:`url(${ book.imageLinks.thumbnail})` }} ></div>
+              ) : (
+                  <div className="book-cover" style={{ width:128, height: 193, backgroundImage: `url('no_thumb.png')` }} ></div>                  
+              )}
               { book.shelf ? (
                   <div className="book-shelf-changer">
                   <select value={ book.shelf} onChange={(event) => this.handleShelfChange(book, event.target.value)}>
@@ -47,7 +51,12 @@ class Book extends React.Component {
             )}
             </div>
             <div className="book-title">{ book.title}</div>
-            <div className="book-authors">{book.authors}</div>
+            { book.authors ? (
+                <div className="book-authors">{book.authors}</div>                
+            ) : 
+                (
+                   <div></div> 
+            ) }
             <div>{this.state.shelf}</div>
           </div>
         );
